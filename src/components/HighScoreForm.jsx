e755b41af0422562b5fb255d82269663065a40c4
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-01 11:35:39 -0600

    setting up post request

--- src/components/HighScoreForm.jsx
+++ src/components/HighScoreForm.jsx
@@ -1,12 +1,16 @@
 import React from 'react'
 
-export default function HighScoreForm() {
+export default function HighScoreForm({cubesCollected}) {
+    function submitHandler(e){
+        e.preventDefault()
+        console.log(e.target.childNodes[1].value)
+    }
     return (
-        <div>
-        <h1 style={{color:'red'}}>You died!</h1>
-        <h2 id='valhalla'>It's ok, you'll be remembered.</h2>
-            <div style={{ backgroundColor: 'lightgreen', margin: 'auto', width: '30%', top: '30%' }}>
-                <form id='highscoreform'>
+        <div id='highscorebackground'>
+            <h1 style={{ color: 'red', marginBottom:'5%', fontSize:'10em'}}>You died!</h1>
+            <h2 id='valhalla'>It's ok, you'll be remembered.</h2>
+            <div style={{ backgroundColor: 'limegreen', margin: 'auto', width: '20%', border:'5px solid lightsteelblue', borderRadius:'5px'}}>
+                <form id='highscoreform' onSubmit={submitHandler}>
                     <label for='name'>Enter your player name!  </label>
                     <input name='name' type='text' maxLength='5'></input>
                     <input type='submit'></input>
