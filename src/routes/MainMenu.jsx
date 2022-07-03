27d522f946260dfe6024f69d56dac4bf959f0716
Sergio Berdeja <sergioberdeja@gmail.com>
2022-07-03 12:25:30 -0600

    main menu styling

--- src/routes/MainMenu.jsx
+++ src/routes/MainMenu.jsx
@@ -6,9 +6,9 @@ export default function MainMenu() {
         <div>
             <h1 id='gametitle'>CubeChaser</h1>
             <h4><em>Gotta collect 'em all!</em></h4>
-            <Link to='scores'>See Scores</Link>
-            <br />
-            <Link to='game'>Play CubeChaser!</Link>
+            <Link to='game' style={{marginTop:'1em', position:'absolute', marginLeft:'-4em'}}><h2>Play CubeChaser!</h2></Link>
+            <button style={{position:'relative', marginTop:'10em'}}>How to Play</button>
+            <Link to='scores' style={{top:'90vh', position:'absolute', marginLeft:'-3em'}}>Highscores</Link>
         </div>
     )
 }
